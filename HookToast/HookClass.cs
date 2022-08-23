91d7366b28c725b1ef3f6df6fb2e4a591f929204
Kazuya Mizuguchi <c33_9858@me.com>
2022-08-23 15:37:10 +0900

    プロジェクト ファイルを追加します。

---
+++ HookToast/HookClass.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+using Windows.UI.Notifications.Management;
+namespace HookToast
+{
+    class HookClass
+    {
+        private static MainPage mainPage;
+        public int interval { get; set; } = 10;
+        public string[] hookTitle { get; set; } = { };
+        public string[] hookBody { get; set; } = { };
+        public HookClass(MainPage mp)
+        {
+            mainPage = mp;
+        }
+        public async void start()
+        {
+
+            UserNotificationListener listener = UserNotificationListener.Current;
+            while (true)
+            {
+                bool playFlg = false; string titleText = ""; string bodyText = "";int toastn = 0;
+                IReadOnlyList<UserNotification> notifs = await listener.GetNotificationsAsync(NotificationKinds.Toast);
+                foreach (var n in notifs)
+                {
+                    NotificationBinding toastBinding = n.Notification.Visual.GetBinding(KnownNotificationBindings.ToastGeneric);
+
+                    if (toastBinding != null)
+                    {
+                        IReadOnlyList<AdaptiveNotificationText> textElements = toastBinding.GetTextElements();
+                        titleText = textElements.FirstOrDefault()?.Text;
+                        bodyText = string.Join("\n", textElements.Skip(1).Select(t => t.Text));
+                        playFlg = checkTitle(titleText, bodyText);
+                        if (playFlg) {
+                            toastn++;
+                        }
+                    }
+                }
+                if (playFlg)
+                {
+                    
+                    mainPage.AlertSountd.Volume = 1;
+                    mainPage.AlertSountd.Play();
+                    mainPage.textbox1.Text = titleText;
+                    mainPage.textbox2.Text = bodyText;
+                    await Task.Delay((int)((interval * 1000) / (toastn * 0.8)));
+                }
+                await Task.Delay(4 * 1000);
+            }
+        }
+        private bool checkTitle(string title,string body)
+        {
+            foreach (string str in hookTitle)
+            {
+                if (title.IndexOf(str) >= 0)
+                {
+                    return true;
+                }
+            }
+            foreach (string str in hookBody)
+            {
+                if (body.IndexOf(str) >= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ HookToast/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Notifications;
+using Windows.UI.Notifications.Management;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace HookToast
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            HookClass hookClass = new HookClass(this);
+            hookClass.hookTitle = new string[] { "ラピッド・ストレージ・テクノロジー・エンタープライズ", "Rapid Storage Technology" };
+            hookClass.hookBody = new string[] { "劣化", "不明", "削除", "詳細を参照"};
+            hookClass.start();
+            bool isFirstActivate = true;
+            Windows.UI.Xaml.Window.Current.Activated += async (s, e) =>
+            {
+                if (e.WindowActivationState == CoreWindowActivationState.CodeActivated
+                    && isFirstActivate)
+                {
+                    isFirstActivate = false;
+
+                    // ウインドウが初めてアクティブになったとき、 CompactOverlay にする。
+                    await ApplicationView.GetForCurrentView()
+                      .TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay);
+                }
+            };
+        }
+    }
+}
